7e194d25ec617107f1b193e6489437a85a7008df
Rellikeht <michaelhemperek@gmail.com>
2024-02-28 16:35:38 +0100

    flake update

--- flake.nix
+++ flake.nix
@@ -1,18 +1,28 @@
 {
   description = "Rellikeht's build of suckless dwm";
 
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    flake-utils.url = github:numtide/flake-utils;
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    system = "x86_64-linux";
-  in {
-    packages.x86_64-linux.default = with import nixpkgs {system = system;};
-      stdenv.mkDerivation rec {
-        name = "dwm";
-        src = self;
+    flake-utils,
+  }:
+    flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "i686-linux"
+      "aarch64-linux"
+      "armv7l-linux"
+    ] (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      name = "tabbed";
+      src = self;
+    in {
+      packages.default = pkgs.stdenv.mkDerivation rec {
+        inherit name system src;
 
         PREFIX = "$(out)";
         CC = pkgs.gcc;
@@ -39,5 +49,5 @@
           make install
           ";
       };
-  };
+    });
 }
