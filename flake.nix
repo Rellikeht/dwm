b38f3c4ef7890ba04baab2bf1a778726f8076aca
Rellikeht <michaelhemperek@gmail.com>
2025-04-21 12:36:22 +0200

    deps update + nixpkgs fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Rellikeht's build of suckless dwm";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
